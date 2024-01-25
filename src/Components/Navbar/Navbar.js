00f8e580f7cc5fc983580401f6336dee35244cef
muhammed shinadh <muhammedshinad2@gmail.com>
2024-01-25 19:56:22 +0530

    first commit

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Navbar/Navbar.css'
+
+function Navbar() {
+  return (
+    <div className='nav'>
+      <img className='logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="netflix logo" />
+
+    </div>
+  )
+}
+
+export default Navbar
