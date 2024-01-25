d209c0a82ffd510c31f35af0ab836e98fb2bc35f
muhammed shinadh <muhammedshinad2@gmail.com>
2024-01-25 20:32:21 +0530

    changes done in scrollbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Banner from './Components/Banner/Banner';
+import Rowpost from './Components/Rowpost/Rowpost';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
      <Banner/>
+     <Rowpost/>
     </div>
   );
 }
