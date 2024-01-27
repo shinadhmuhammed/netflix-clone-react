851bfdcce58e21d01045eea0a497ddaa39e5d1f8
muhammed shinadh <muhammedshinad2@gmail.com>
2024-01-27 12:54:30 +0530

    add to play video when that specific image clicked

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,34 +1,49 @@
 import React, { useEffect, useState } from "react";
 import axios from '../../axios';
 import "./Banner.css";
-import {apiKey,imageUrl} from '../../constants/constants'
-
+import { apiKey, imageUrl } from '../../constants/constants';
+import YouTube from 'react-youtube';
 
 function Banner() {
+  const [movie, setMovie] = useState(null);
+  const [urlId, setUrlId] = useState('');
 
-  const[movie,setMovie]=useState()
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${apiKey}&language=en-US`).then((response) => {
+      setMovie(response.data.results[0]);
+    });
+  }, []);
 
-  useEffect(()=>{
-    axios.get(`trending/all/week?api_key=${apiKey}&language=en-US`).then((response)=>{
-    console.log(response.data);
-    setMovie(response.data.results[0])
-  })
-  },[])
-    
+  const handlePlayClick = () => {
+    axios.get(`/movie/${movie.id}/videos?language=en-US&api_key=${apiKey}&language=en-US`).then(response => {
+      if (response.data.results.length > 0) {
+        setUrlId(response.data.results[0]);
+      } else {
+        console.log('Trailer is empty');
+      }
+    });
+  };
 
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 0, 
+    },
+  };
 
   return (
-    <div style={{backgroundImage:`url(${movie? imageUrl+movie.backdrop_path:""})`}}
-     className="banner">
+    <div style={{backgroundImage: `url(${movie ? imageUrl + movie.backdrop_path : ""})`}} className="banner">
       <div className="content">
-        <h1 className="title">{movie?movie.title:''}</h1>
+        <h1 className="title">{movie ? movie.title : ''}</h1>
         <div className="banner-buttons">
-          <button className="button">play</button>
+          <button className="button" onClick={handlePlayClick}>Play</button>
           <button className="button">My List</button>
         </div>
-        <h1 className="description">{movie?movie.overview:""}</h1>
+        <h1 className="description">{movie ? movie.overview : ""}</h1>
       </div>
       <div className="fade"></div>
+      {urlId && <YouTube opts={opts} videoId={urlId.key} />}
     </div>
   );
 }
--- src/Components/Rowpost/Rowpost.js
+++ src/Components/Rowpost/Rowpost.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "./Rowpost.css";
 import axios from "../../axios";
-import { imageUrl } from "../../constants/constants";
+import { imageUrl,apiKey} from "../../constants/constants";
+import YouTube from 'react-youtube'
 
 function Rowpost(props) {
   const [movies, setMovies] = useState([]);
+  const[urlId,setUrlId]=useState('')
+
 
   useEffect(() => {
     axios
@@ -15,21 +18,42 @@ function Rowpost(props) {
       .catch((err) => {
         alert("error happening");
       });
-  }, []);
-  
+  });
+
+
+  const opts = {
+    height: '390',
+    width: '100%',
+    playerVars: {
+      autoplay: 0,
+    }
+  }
+
+  const handleMovieTrailer=(id)=>{
+    axios.get(`/movie/${id}/videos?language=en-US&api_key=${apiKey}&language=en-US`).then(response=>{
+      if(response.data.results!==0){
+        setUrlId(response.data.results[0])
+      }else{
+        console.log('tailer is empty');
+      }
+    })
+  }
+
+
 
   return (
     <div className="row">
       <h2>{props.title}</h2>
       <div className="posters">
         {movies.map((obj) => (
-          <img
+          <img onClick={()=>handleMovieTrailer(obj.id)}
             className={props.isSmall ? "smallImg" : "img"}
             src={`${imageUrl + obj.backdrop_path}`}
             alt="cards"
           />
         ))}
       </div>
+     { urlId && <YouTube opts={opts} videoId={urlId.key}/>}
     </div>
   );
 }
